a84afc6fb34e6ab781fde6ddf6b8ed501ac8f078
earthsirapob <sirapob.jarutanyaluk@gmail.com>
2024-07-24 11:55:58 +0700

    update

---
+++ src/payload.config.ts
@@ -0,0 +1,35 @@
+import path from 'path'
+
+import { payloadCloud } from '@payloadcms/plugin-cloud'
+import { postgresAdapter } from '@payloadcms/db-postgres'
+import { webpackBundler } from '@payloadcms/bundler-webpack'
+import { slateEditor } from '@payloadcms/richtext-slate'
+import { buildConfig } from 'payload/config'
+
+import Users from './collections/Users'
+
+export default buildConfig({
+  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
+  admin: {
+    user: Users.slug,
+    bundler: webpackBundler(),
+  },
+  editor: slateEditor({}),
+  collections: [Users],
+  typescript: {
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+  },
+  graphQL: {
+    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+  },
+  plugins: [payloadCloud()],
+  db: postgresAdapter({
+    pool: {
+      host: '192.168.0.13',
+      port: 5432,
+      user: 'postgres',
+      password: '1234',
+      database: 'kpc-gwl-cms',
+    },
+  }),
+})
